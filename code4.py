bdf0b3ab9aedd47b2e675afa2f50eb7180ba21f7
PreetiGanganalli <preetiganganallisai@gmail.com>
2016-01-27 17:10:06 +0530

    github code

---
+++ code4.py
@@ -0,0 +1,11 @@
+phrase = "the surprise is in here somewhere" 
+print phrase.find("surprise")
+print phrase.find("ejafjallajkull") 
+print "the surprise is in here somewhere".find("surprise") 
+print "I put a string in your string".find("string")
+
+
+
+
+
+
